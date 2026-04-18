f7b77b887943071564d1947682f5d0d5064dbe13
drakon660 <drakon660@gmail.com>
2026-04-18 22:14:53 +0200

    updates

--- Sample-Library/Library.Components/StateMachines/Reservation.cs
+++ Sample-Library/Library.Components/StateMachines/Reservation.cs
@@ -7,6 +7,7 @@ public class Reservation : SagaStateMachineInstance
     public Guid CorrelationId { get; set; }
     public Guid MemberId { get; set; }
     public DateTime Created { get; set; }
+    public DateTime? Reserved { get; set; }
     public int CurrentState { get; set; }
     public Guid BookId { get; set; }
 }
--- Sample-Library/Library.Components/StateMachines/ReservationStateMachine.cs
+++ Sample-Library/Library.Components/StateMachines/ReservationStateMachine.cs
@@ -9,16 +9,25 @@ public class ReservationStateMachine : MassTransitStateMachine<Reservation>
     {
         Event(() => ReservationRequested, x 
             => x.CorrelateById(m => m.Message.ReservationId));
+        
         InstanceState(x => x.CurrentState, Requested);
         Initially(
             When(ReservationRequested).Then(UpdateSagaFromMessage)
                 .TransitionTo(Requested));
+        
+        During(Requested,When(BookReserved)
+            .Then((context) =>
+            {
+                context.Saga.Reserved = context.Message.Timestamp;
+            }) 
+            .TransitionTo(Reserved));
     }
 
     public State Requested { get; set; }
     public State Reserved { get; set; }
 
     public Event<ReservationRequested> ReservationRequested { get; set; }
+    public Event<BookReserved> BookReserved { get; set; }
     
     private void UpdateSagaFromMessage(BehaviorContext<Reservation, ReservationRequested> saga)
     {
